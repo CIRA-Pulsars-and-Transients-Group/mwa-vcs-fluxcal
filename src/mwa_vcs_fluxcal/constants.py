96185d503621a4614a4aeabd50c69a993748ec21
Bradley Meyers <bradley.meyers1993@gmail.com>
2025-02-03 14:52:33 +0800

    change to just having the file name as a package constant

--- src/mwa_vcs_fluxcal/constants.py
+++ src/mwa_vcs_fluxcal/constants.py
@@ -5,7 +5,6 @@
 import pkg_resources
 from astropy import units as u
 from astropy.coordinates import EarthLocation
-from healpy import read_map
 
 __all__ = [
     "MWA_CENTRE_LON",
@@ -13,7 +12,7 @@ __all__ = [
     "MWA_CENTRE_H",
     "MWA_CENTRE_CABLE_LEN",
     "MWA_LOCATION",
-    "SKY_TEMP_MAP",
+    "SKY_TEMP_MAP_FILE",
     "RCVR_TEMP_FILE",
 ]
 
@@ -27,8 +26,7 @@ MWA_LOCATION = EarthLocation.from_geodetic(
 )
 
 # Get package data files (mildly hacky?)
-SKY_TEMP_MAP = read_map(
-    pkg_resources.resource_filename("mwa_vcs_fluxcal", "data/haslam408_ds_Remazeilles2014.fits"),
-    dtype=None,
+SKY_TEMP_MAP_FILE = pkg_resources.resource_filename(
+    "mwa_vcs_fluxcal", "data/haslam408_ds_Remazeilles2014.fits"
 )
 RCVR_TEMP_FILE = pkg_resources.resource_filename("mwa_vcs_fluxcal", "data/MWA_Trcvr_tile_56.csv")

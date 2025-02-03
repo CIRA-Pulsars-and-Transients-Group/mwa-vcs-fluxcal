82bb146e730ba830d9e8832a94fd4214d263043e
Bradley Meyers <bradley.meyers1993@gmail.com>
2025-02-03 14:53:52 +0800

    adjusted how sky temp. map is loaded, now use metafits file context to extract frequency information, minor type-hinting updates

--- src/mwa_vcs_fluxcal/temperatures.py
+++ src/mwa_vcs_fluxcal/temperatures.py
@@ -7,12 +7,13 @@ import logging
 import numpy as np
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
+from healpy import read_map
 from healpy.pixelfunc import get_interp_val
 from mwalib import MetafitsContext
 from scipy.interpolate import CubicSpline
 
 import mwa_vcs_fluxcal
-from mwa_vcs_fluxcal import RCVR_TEMP_FILE, SKY_TEMP_MAP
+from mwa_vcs_fluxcal import RCVR_TEMP_FILE, SKY_TEMP_MAP_FILE
 
 __all__ = ["splineSkyTempAtCoord", "splineRecieverTemp"]
 
@@ -43,6 +44,9 @@ def splineSkyTempAtCoord(
     if logger is None:
         logger = mwa_vcs_fluxcal.get_logger()
 
+    # read the sky temperature map
+    sky_temp_map = read_map(SKY_TEMP_MAP_FILE)
+
     # convert ra, dec to Galactic coordinate
     logger.info("Converting pointing coordinate to Galactic frame")
     gal = coord.transform_to("galactic")
@@ -51,13 +55,13 @@ def splineSkyTempAtCoord(
 
     # retrieve sky temperature from sky map
     logger.info(f"Estimating Tsky at (l, b) = ({gl}, {gb}) deg")
-    map_tsky = get_interp_val(SKY_TEMP_MAP, gl, gb, nest=False, lonlat=True)
+    map_tsky = get_interp_val(sky_temp_map, gl, gb, nest=False, lonlat=True)
 
     # scale sky temperature to center frequency of the survey
     logger.info("Interpolating Tsky across observing bandwdith")
     map_freq_mhz = 408.0
-    # TODO: update this to inspect context to figure our the frequency span
-    freqs = np.linspace(140, 170, 100)
+    obs_freq_mhz = context.metafits_fine_chan_freqs_hz / 1e6
+    freqs = np.linspace(min(obs_freq_mhz), max(obs_freq_mhz), 100)
     tsky_sample = map_tsky * (freqs / map_freq_mhz) ** sky_index
 
     # compute a cubic spline interpolation
@@ -66,13 +70,9 @@ def splineSkyTempAtCoord(
     return tsky_spline
 
 
-def splineRecieverTemp(context: MetafitsContext):
+def splineRecieverTemp() -> CubicSpline:
     """Get receiver temperature from the receiver temperature (Trcvr) file, returning
     a CubicSpline for interpolation across the full MWA band
-
-    :param context: A mwalib.MetafitsContext object that contains the
-                    array configuration and more importantly, frequency selections.
-    :type context: MetafitsContext
 
     :return: A cubic spline interpolation object based on the sampled Trcvr data.
              Input into the spline object must be in MHz for correct temperatures in Kelvin.
